71b59609deb19b81aa8429ea4b6ec024bda32850
Таисия Хомчук <homcuktaisia@gmail.com>
2026-01-16 14:48:09 +0500

    Add initial models, admin and index page with table

--- analyzer/views.py
+++ analyzer/views.py
@@ -1,17 +1,33 @@
 from django.shortcuts import render
-from .models import ProjectTechnology  # твоя модель
-from collections import Counter
+from .models import ProjectTechnology
+import pandas as pd
+import plotly.express as px
+import plotly.io as pio
 
 def index(request):
-    # Получаем все объекты из модели
-    project_techs = ProjectTechnology.objects.all()
+    # Получаем данные
+    project_techs = ProjectTechnology.objects.all().values('technology__name')
+    df = pd.DataFrame(list(project_techs))
 
-    # Создаем список технологий
-    all_technologies = [pt.technology.name for pt in project_techs]  # pt.technology.name - строка
+    if not df.empty:
+        # Считаем количество проектов по технологии
+        technology_stats = df['technology__name'].value_counts().to_dict()
 
-    # Считаем, сколько раз встречается каждая технология
-    technology_stats = dict(Counter(all_technologies))  # преобразуем в словарь
+        # Создаём график
+        fig = px.bar(
+            x=list(technology_stats.keys()),
+            y=list(technology_stats.values()),
+            labels={'x':'Технология', 'y':'Количество проектов'},
+            title='Популярность технологий в проектах'
+        )
+
+        # Преобразуем график в HTML
+        graph_html = pio.to_html(fig, full_html=False)
+    else:
+        technology_stats = {}
+        graph_html = "<p>Нет данных для построения графика</p>"
 
     return render(request, 'analyzer/index.html', {
-        'technology_stats': technology_stats
+        'technology_stats': technology_stats,
+        'graph_html': graph_html
     })
